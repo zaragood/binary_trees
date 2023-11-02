518f68436ff71baedab6bfbd1b39c465ed9fef03
zaragood <azaragoodness52@gmail.com>
2023-11-02 06:37:13 -0700

    betty check

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -18,7 +18,7 @@ size_t binary_tree_nodes(const binary_tree_t *tree)
 	if (tree->left != NULL || tree->right != NULL)
 	{
 		/* If the node has at least one child, return 1 */
-		return ( 1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
+		return (1 + binary_tree_nodes(tree->left) + binary_tree_nodes(tree->right));
 	}
 
 	/* If the node has no children, return 0 */
