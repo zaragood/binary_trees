56084bc8c4c2e6b7ffe2ea27c9d20c0e19da739e
zaragood <azaragoodness52@gmail.com>
2023-11-02 07:06:22 -0700

    betty check

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -1,7 +1,9 @@
 #include "binary_trees.h"
 /**
- * binary_tree_balance - function that measures the balance factor of a binary tree
- * @tree: tree is a pointer to the root node of the tree to measure the balance factor
+ * binary_tree_balance - function that measures the balance factor
+ * of a binary tree
+ * @tree: tree is a pointer to the root node of the tree to measure
+ * the balance factor
  * Return: If tree is NULL, return 0
  */
 
@@ -28,16 +30,16 @@ int binary_tree_balance(const binary_tree_t *tree)
 
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-        size_t left_height, right_height;
+	size_t left_height, right_height;
 
-        /* Base case: If tree is NULL, height is 0 */
-        if (tree == NULL)
-                return (0);
+	/* Base case: If tree is NULL, height is 0 */
+	if (tree == NULL)
+		return (0);
 
-        /*Recursively calculate height of left and right subtrees*/
-        left_height = binary_tree_height(tree->left);
-        right_height = binary_tree_height(tree->right);
+	/*Recursively calculate height of left and right subtrees*/
+	left_height = binary_tree_height(tree->left);
+	right_height = binary_tree_height(tree->right);
 
-        /*Return the height of the taller subtree, plus 1 for the current node*/
-        return (1 + (left_height > right_height ? left_height : right_height));
+	/*Return the height of the taller subtree, plus 1 for the current node*/
+	return (1 + (left_height > right_height ? left_height : right_height));
 }
